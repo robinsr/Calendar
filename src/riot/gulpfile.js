ebc8f273006dfa2f43941994b22bd6ac0bfdfc22
Ryan Robinson <ryan.b.robinson@gmail.com>
2016-04-10 14:54:10 -0700

    Injecting items data in riot

--- src/riot/gulpfile.js
+++ src/riot/gulpfile.js
@@ -2,16 +2,27 @@ var gulp = require('gulp');
 var inject = require('gulp-inject');
 var watch = require('gulp-watch');
 
+var riotTrans = function( path ) {
+    return '<script src="src/' + path + '" type="riot/tag"></script>';
+};
+
+var jsTrans = function ( path ) {
+    return '<script src="src/' + path + '"></script>';
+};
+
+var jsonTrans = function ( file ) {
+    var contents = file.contents.toString('utf8');
+    return '<script tpye="application/json">APP_DATA=' + contents + '</script>'; 
+}
+
 gulp.task('inject', function () {
     var sources = gulp
-    .src(['./src/**/*.{tag,js}'], {read: false});
+    .src(['./src/**/*.{tag,js}', '../common/items.json']);
 
-    var transform = function (filepath, file, i, length) {
-        if ( /\.tag/.test( filepath ) ) {
-            return '<script src="src/' + filepath + '" type="riot/tag"></script>';
-        } else {
-            return '<script src="src/' + filepath + '"></script>';
-        }
+    var transform = function (path, file, i, length) {
+        if ( /\.tag/.test( path ) ) return riotTrans( path );
+        if ( /\.js$/.test( path ) ) return jsTrans( path );
+        if ( /\.json$/.test( path ) ) return jsonTrans( file );
     };
 
     var injectOpts = { 
