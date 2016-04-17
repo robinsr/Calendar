467f1daae6c9ea89499bc94eeb6a0f1dd36980fd
Ryan Robinson <ryan.b.robinson@gmail.com>
2016-04-17 16:32:44 -0700

    Added flux

--- src/react/app/components/App.jsx
+++ src/react/app/components/App.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
-import Note from './Note.jsx';
+
+import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-    render () {
-        return <Note />;
+  constructor ( props ) {
+    super( props );
+
+    this.state = NoteStore.getState();
+  }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+
+  storeChanged = (state) => {
+    this.setState(state);
+  }
+
+  render () {
+    const notes = this.state.notes;
+
+    return (
+      <div>
+      <button className="add-note" onClick={this.addNote}>+</button>
+      <Notes 
+        notes={notes}
+        onEdit={this.editNote}
+        onDelete={this.deleteNote}/>
+      </div>
+      );
+  }
+
+  addNote = () => {
+    NoteActions.create({task: 'New Task'});
+  };
+
+  editNote = (id, task) => {
+    if (!task.trim()) {
+      return;
     }
+
+    NoteActions.update({id, task});
+  };
+
+  deleteNote = (id, e) => {
+    e.stopPropagation();
+    
+    NoteActions.delete(id);
+  };
 }
