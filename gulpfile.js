53c300b9777c295bef5ce226e5003ee620917881
Ryan Robinson <ryan.b.robinson@gmail.com>
2016-04-26 15:15:04 -0700

    Added gulp hub. Backbone done. common sass done. todo: angular, vanilla, react-ebpack

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var hub = require('gulp-hub');
+
+gulp.task('default', function () {
+  return hub([
+    './src/common/gulpfile.js',
+    './src/backbone/gulpfile.js'
+  ]);
+})
--- src/common/gulpfile.js
+++ src/common/gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 
 var sassOpt = {outputStyle: 'compressed'};
 
-gulp.task('build-styles', function () {
+gulp.task('default', function () {
   return gulp.src('./*.scss')
     .pipe(sass(sassOpt).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
