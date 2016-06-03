89c4907cd486e2fd712af5e38a13394b2dd76055
Ryan Robinson <ryan.b.robinson@gmail.com>
2016-06-03 16:10:52 -0700

    Updated angular to use node service

--- src/angular/src/directives/drag-item.js
+++ src/angular/src/directives/drag-item.js
@@ -7,7 +7,7 @@ angular.module( 'calendar' ).directive( 'dragItem', function () {
   return { 
     link: function ( scope, elem ) {
       $( elem ).on( 'dragstart', function ( e ) {
-        e.originalEvent.dataTransfer.setData( 'calendar', scope.item.$$hashKey );
+        e.originalEvent.dataTransfer.setData( 'calendar', scope.item.id );
       } );
     }
   };
